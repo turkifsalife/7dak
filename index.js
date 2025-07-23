0cf1f32a184c004cfaeb9d05b2c134fbcdff1578
turkifsalife <turkifsalife@gmail.com>
2025-07-23 19:17:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ app.use(
   createProxyMiddleware({
     target: 'https://7dak.fun',
     changeOrigin: true,
+    headers: {
+      host: '7dak.fun',
+      'user-agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+    },
     onProxyReq: (proxyReq) => {
       proxyReq.removeHeader('x-forwarded-host');
-      proxyReq.removeHeader('host');
     },
   })
 );
