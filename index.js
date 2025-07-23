d331903c39b6fcb9ba31fd0e9095f7bd9bc81788
turkifsalife <turkifsalife@gmail.com>
2025-07-23 19:12:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const express = require('express');
 const app = express();
 
-app.use('/', createProxyMiddleware({
-  target: 'https://7dak.fun',
-  changeOrigin: true,
-  onProxyReq(proxyReq, req, res) {
-    proxyReq.setHeader('x-forwarded-host', req.headers.host);
-  }
-}));
+app.use(
+  '/',
+  createProxyMiddleware({
+    target: 'https://7dak.fun', // Hedef siten
+    changeOrigin: true,
+    onProxyReq: (proxyReq, req, res) => {
+      // Sonsuz döngü yaratabilecek header'ları temizle
+      proxyReq.removeHeader('x-forwarded-host');
+      proxyReq.removeHeader('host');
+    }
+  })
+);
 
 module.exports = app;
